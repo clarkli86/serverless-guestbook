b527c96f9e2e00101a537a023c93dde5923053f8
Clark Li <clark.li86@gmail.com>
2021-03-06 23:56:47 +1030

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://652319e9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
